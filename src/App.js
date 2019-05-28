cd9d2289f63f5c536742d971b2f592f7d51cfc10
Andrey Kashapov <kashapovandrey@gmail.com>
2019-05-28 16:45:57 +0300

    event trigger

--- src/App.js
+++ src/App.js
@@ -2,12 +2,23 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 
 class App extends Component {
+  addTrack() {
+    console.log("add track:", this.trackInput.value);
+    this.props.onAddTrack(this.trackInput.value);
+    this.trackInput.value = "";
+  }
+
   render() {
-    console.log(this.props.testStore);
+    console.log("store: ", this.props.testStore);
     return (
       <div>
-        <input type="text" />
-        <button>Add track</button>
+        <input
+          type="text"
+          ref={input => {
+            this.trackInput = input;
+          }}
+        />
+        <button onClick={this.addTrack.bind(this)}>Add track</button>
         <ul>
           {this.props.testStore.map((track, index) => (
             <li key={index}>{track}</li>
@@ -22,5 +33,9 @@ export default connect(
   state => ({
     testStore: state
   }),
-  dispatch => ({})
+  dispatch => ({
+    onAddTrack: trackName => {
+      dispatch({ type: "ADD_TRACK", payload: trackName });
+    }
+  })
 )(App);
